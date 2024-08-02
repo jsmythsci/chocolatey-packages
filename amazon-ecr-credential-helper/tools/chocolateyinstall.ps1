d2034e02a8a1952172d100c2f7a2febee235f7a8
Jason Smyth <jsmyth@taqauto.com>
2024-08-02 16:37:26 -0400

    amazon-ecr-credential-helper v0.8.0

--- amazon-ecr-credential-helper/tools/chocolateyinstall.ps1
+++ amazon-ecr-credential-helper/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
     FileFullPath = $fileFullPath
 
     # 64-bit
-    url64          = 'https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/0.7.1/windows-amd64/docker-credential-ecr-login.exe'
-    checksum64     = '60dd759f6c333ac12b5447470a4e26553d39c9288765771e5e264daa135f46b2'
+    url64          = 'https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/0.8.0/windows-amd64/docker-credential-ecr-login.exe'
+    checksum64     = 'eb385f5c69dd62087a9c59cbade8feeb47bd9c8ec2addb84b4427dda8d704201'
     checksumType64 = 'sha256'
 }
 
